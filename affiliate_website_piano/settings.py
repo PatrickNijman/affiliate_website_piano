b586d01b3646187e0f7461e81fa70a81893b1d5e
PatrickNijman <nijman.patrick@gmail.com>
2022-08-28 21:22:31 +0200

    jg

--- affiliate_website_piano/settings.py
+++ affiliate_website_piano/settings.py
@@ -122,8 +122,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
